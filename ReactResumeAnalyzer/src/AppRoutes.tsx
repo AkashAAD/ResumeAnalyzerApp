b533f35f79eb76ac5c7fd4870d30486171ff0ffe
Akash D <deshbhratarakash@gmail.com>
2025-04-21 19:08:48 +0530

    inital commit

---
+++ ReactResumeAnalyzer/src/AppRoutes.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SignIn from "./Components/SignIn/SignIn";
+import SignUp from "./Components/SignUp/SignUp";
+import ResumeAnalyzer from "./Components/ResumeAnalyzer/ResumeAnalyzer";
+
+const AppRoutes = () => {
+  return (
+    <>
+      <BrowserRouter>
+        
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default AppRoutes;
+
---
+++ ReactResumeAnalyzer/src/Components/ResumeAnalyzer/ResumeAnalyzer.tsx
@@ -0,0 +1,7 @@
+const ResumeAnalyzer = () => {
+  return (
+    <>ResumeAnalyzer</>
+  )
+}
+
+export default ResumeAnalyzer;
